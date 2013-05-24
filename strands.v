b9d75047d3408f410b54781b3a691cfe0b4799f7
Andrew Kent <amk.kent@gmail.com>
2013-05-24 10:35:30 -0600

    bundle edge property proofs started (Lemma 2.7 work)

--- strands.v
+++ strands.v
@@ -17,7 +17,8 @@
    http://www.mitre.org/work/tech_papers/tech_papers_01/guttman_bundles/
  *)
 
-Require Import List Arith Peano_dec Omega Ensembles Finite_sets.
+Require Import List Arith Peano_dec Omega Ensembles Finite_sets Relation_Definitions
+ProofIrrelevance.
 
 (* Represent atomic messages, *)
 Variable Text : Set.
@@ -55,7 +56,7 @@ Inductive Subterm : Msg -> Msg -> Prop :=
 | st_app_r : forall st l r, 
                Subterm st r -> Subterm st (msg_app l r)
 | st_crpyt : forall st t k, 
-              Subterm st t -> Subterm st (msg_crypt k t).
+               Subterm st t -> Subterm st (msg_crypt k t).
 (* [REF 1] Definition 2.1 pg 6 and Definition 2.11 *)
 Hint Constructors Subterm.
 
@@ -87,20 +88,21 @@ Inductive StrandSpace : Type :=
 | strandspace : Msgs -> Strands -> StrandSpace.
 (* [REF 1] Definition 2.2 pg 6 "A strand space over A (set of possible msgs) 
            is a set E with a trace mapping tr : E -> list smsg" *)
+Hint Constructors StrandSpace.
 
 Definition SS_msgs (ss:StrandSpace) : Msgs :=
- match ss with
-  | strandspace m_set s_set => m_set
- end.
+  match ss with
+    | strandspace m_set s_set => m_set
+  end.
 
 Definition SS_strands (ss:StrandSpace) : Strands :=
- match ss with
-  | strandspace m_set s_set => s_set
- end.
+  match ss with
+    | strandspace m_set s_set => s_set
+  end.
 
 
 (* node in a strand space *)
-Definition Node : Type := {n: (prod Strand nat) | (snd n) < (length (fst n))}.
+Definition Node : Type := {n: (Strand * nat) | (snd n) < (length (fst n))}.
 (* [REF 1] Definition 2.3.1 pg 6
    -"A node is a pair <s,i> where s is a strand and i a nat in [0, (length s))"
      NOTE: I changed it to be 0 based instead of 1 based sequences
@@ -139,7 +141,7 @@ Fixpoint Node_smsg_option (n:Node) : (option SMsg) :=
    "Define term(n) to be the ith signed term of the trace of s." *)
 
 Lemma nth_error_len : forall {X:Type} {l:list X} i,
-None = nth_error l i -> (length l) <= i.
+                        None = nth_error l i -> (length l) <= i.
 Proof.
   intros X l i. generalize dependent l.
   induction i. 
@@ -158,8 +160,8 @@ Proof.
   simpl. omega. 
 Qed.
 
-Theorem Node_smsg_valid : forall (n:Node),
-{m:SMsg | Some m = Node_smsg_option n}.
+Theorem node_smsg_valid : forall (n:Node),
+                            {m:SMsg | Some m = Node_smsg_option n}.
 (* exists (m:smsg), Some m = (n_smsg_option n). *)
 Proof.
   intros n.
@@ -175,9 +177,9 @@ Qed.
 
 (* signed message of a node *)
 Definition Node_smsg (n:Node) : SMsg :=
- match Node_smsg_valid n with
-     | exist m _ => m
- end.
+  match node_smsg_valid n with
+    | exist m _ => m
+  end.
 
 (* unsigned message of a node *)
 Fixpoint Node_msg (n:Node) : Msg :=
@@ -189,34 +191,80 @@ Fixpoint Node_msg (n:Node) : Msg :=
    "Define uns_term(n) to be the unsigned part of the ith signed term 
     of the trace of s." *)
 
-(* To reason about the set of nodes in a strand space *)
-Definition NodeInSS (n:Node) (ss:StrandSpace) : Prop := 
- In Strand (SS_strands ss) (Node_strand n).
-(* [REF 1] Definition 2.3.1 pg 6
-   The reference to the "set of nodes (N) in a given strand space." *)
+Theorem node_eq_iff : forall n m,
+n = m <-> (Node_strand n = Node_strand m /\ Node_index n = Node_index m).
+Proof.
+  intros.
+  destruct n as [[ns ni] np].
+  destruct m as [[ms mi] mp].
+  simpl.
+  split.
 
-(* Set of all nodes in a strand space *)
-Definition NodeSet (ss:StrandSpace) : Type := 
-{ns: Nodes | forall n, (NodeInSS n ss)  <-> In Node ns n}.
-(* [REF 1] Definition 2.3.1 pg 6
-   The set of nodes (in a strand space) is denoted by N. *)
+  intros Heq.
+  inversion Heq.
+  auto.
+
+  intros Heq.
+  inversion Heq; subst.
+  inversion Heq.
+  assert (np = mp). apply proof_irrelevance.
+  subst. auto.
+Qed.
+Hint Resolve node_eq_iff.
+
 
 Inductive CommEdge : Node -> Node -> Prop :=
-| c_edge :  forall n m, (exists a, (and ((Node_smsg n) = (tx a)) 
-                                 ((Node_smsg m) = (rx a))))
-                             -> CommEdge n m.
+| c_edge :  forall n m, ((exists a, (Node_smsg n = tx a 
+                                    /\ Node_smsg m = rx a))
+                        /\ Node_strand n <> Node_strand m)
+                        -> CommEdge n m.
 (* [REF 1] Definition 2.3.3 pg 6
-   "there is an edge n1 -> n2 iff term(n1) = +a and term(n2) = -a." *)
+   "there is an edge n1 -> n2 iff term(n1) = +a and term(n2) = -a ... 
+   recording a potential causal link between those strands**"
+  **We take this to mean a causal link between two *different* 
+  strands, as this is logical, and fits numerous informal 
+  descriptions in the literature such as "A strand (process) 
+  may send or receive a message but not both at the 
+  same time" and given the majority of transmissions travel 
+  close to the speed of light a strand cannot receive
+  its own transmission by any reasonable measure. *)
+Hint Constructors CommEdge.
+
+
+Lemma c_edge_nrefl : forall n m,
+CommEdge n m -> n <> m.
+Proof.
+  intros.
+  destruct H. destruct H.
+  intros contra. apply node_eq_iff in contra.
+  inversion contra as [contra_s contra_i].
+  apply H0. auto.
+Qed.  
+Hint Resolve c_edge_nrefl.
+
 
 (* predecessor edge *)
 (* node's direct predecessor -> node -> Prop *)
 Inductive PredEdge : Node -> Node -> Prop :=
 | p_edge : forall i j, Node_strand i = Node_strand j 
-                      -> (Node_index i) + 1 = (Node_index j) 
-                      -> PredEdge i j.
+                       -> (Node_index i) + 1 = Node_index j 
+                       -> PredEdge i j.
 (* [REF 1] Definition 2.3.4 pg 6
    "When n1= <s,i> and n2=<s,i+1> are members of N (set of node), there is
     an edge n1 => n2." *)
+
+Lemma p_edge_nrefl : forall n m,
+PredEdge n m -> n <> m.
+Proof.
+  intros.
+  destruct H.
+  intros contra. apply node_eq_iff in contra.
+  inversion contra as [contra_s contra_i].
+  rewrite <- H0 in contra_i. 
+  destruct (Node_index i). omega.  omega. 
+Qed.  
+Hint Resolve p_edge_nrefl.
+
 
 (* predecessor multi edge (not nec. immediate predecessor) *)
 (* node's predecessor -> node -> Prop *)
@@ -226,14 +274,7 @@ Inductive PredPath : Node -> Node -> Prop :=
 (* [REF 1] Definition 2.3.4 pg 6
    "ni =>+ nj means that ni precedes nj (not necessarily immediately) on
     the same strand." *)
-
-(* A Path (traversing either edge Types) of length
-   1 or more. *)
-Inductive FwdPath : Node -> Node -> Prop :=
-| path_comm : forall x y, CommEdge x y -> FwdPath x y
-| path_pred : forall x y, PredPath x y -> FwdPath x y
-| path_comm_path : forall x y z, CommEdge x y -> FwdPath y z -> FwdPath x z
-| path_pred_path : forall x y z, PredPath x y -> FwdPath y z -> FwdPath x z.
+Hint Constructors PredPath.
 
 Inductive OccursIn : Msg -> Node -> Prop :=
 | occurs_in : forall m n, Subterm m (Node_msg n) -> OccursIn m n.
@@ -242,12 +283,12 @@ Inductive OccursIn : Msg -> Node -> Prop :=
 
 (* signifies the origin of a msg. *)
 Inductive EntryPoint : Node -> Msgs -> Prop :=
- | entrypoint :  forall n I, 
-    (and (exists m, (and ((Node_smsg n) = (tx m)) 
-                         (In Msg I m)))
-         (forall n', (PredPath n' n) 
-                     -> ~(In Msg I (Node_msg n'))))
-    -> EntryPoint n I.
+| entrypoint :  forall n I, 
+                  ((exists m, ((Node_smsg n = tx m) 
+                               /\ (In Msg I m)))
+                   /\ (forall n', (PredPath n' n) 
+                                  -> ~(In Msg I (Node_msg n'))))
+                  -> EntryPoint n I.
 (* [REF 1] Definition 2.3.6 pg 6
    "The node n in N is an entry point for I (a set of unsigned terms) iff
     term(n) = +t for some t in I, and whenever n' =>+ n term(n') is not in I."*)
@@ -255,9 +296,9 @@ Inductive EntryPoint : Node -> Msgs -> Prop :=
 (* where an unsigned term originates, what node *)
 Inductive OriginateAt : Msg -> Node -> Prop :=
 | orig_at : forall m n, 
-             (exists I, (and (EntryPoint n I)
-                             (forall m', In Msg I m -> Subterm m m')))
-             -> OriginateAt m n.
+              (exists I, ((EntryPoint n I)
+                          /\ (forall m', In Msg I m -> Subterm m m')))
+              -> OriginateAt m n.
 (* [REF 1] Definition 2.3.7 pg 6
    "An unsigned term t originates on n in N iff n is an entry point for 
     the set I = {t' | t is a subterm of t'}."*)
@@ -265,48 +306,127 @@ Inductive OriginateAt : Msg -> Node -> Prop :=
 (* uniquely originating term def, useful for nonces or session keys *)
 Inductive UniqOrigin : Msg -> Prop :=
 | uniq_orig : forall t, (exists n, 
-              (forall n', OriginateAt t n' -> n = n') )
-              -> UniqOrigin t.
+                           (forall n', OriginateAt t n' -> n = n') )
+                        -> UniqOrigin t.
 (* [REF 1] Definition 2.3.8 pg 7 
    "unsigned term t is uniquely originating iff t originates on a unique
     n in N." *)
 
+(* In for members of pairs *)
 Inductive InP (X:Type) : Ensemble (X*X) -> X -> Prop :=
 | inp_l : forall E x,
-           (exists y, In (X*X) E (x,y))
-           -> InP X E x
+            (exists y, In (X*X) E (x,y))
+            -> InP X E x
 | inp_r : forall E x,
-           (exists y, In (X*X) E (y,x))
-           -> InP X E x.
+            (exists y, In (X*X) E (y,x))
+            -> InP X E x.
+Hint Constructors InP.
 
-Inductive ValidNodes : Nodes -> Prop :=
-| vn : forall N, Finite Node N -> ValidNodes N.
+Inductive ValidNodes : Nodes -> Edges -> Prop :=
+| vn : forall N E,
+         Finite Node N ->
+         (forall x y, In Node N x ->
+                      In Node N y -> 
+                      CommEdge x y \/ PredPath x y ->
+                      In Edge E (x,y)) ->
+         ValidNodes N E.
 
-Inductive ValidEdges : Nodes -> Edges -> Prop :=
-| ve : forall N E,
-         (and (Finite Edge E) 
-              (forall n, InP Node E n -> In Node N n)) ->
-         ValidEdges N E.
 
-Inductive Acyclic : Nodes -> Prop :=
-| acyclic : forall N,
- (forall n, (and (In Node N n) 
-                (~ FwdPath n n))) -> Acyclic N.
+(*Ensures edges are finite, built from nodes in the node set
+  and meet the given constraints (Comm or Pred) *)
+Inductive ValidEdges : (Node -> Node -> Prop) -> Nodes -> Edges -> Prop :=
+| ve : forall (P: Node -> Node -> Prop) N E,
+         (Finite Edge E) 
+         /\ (forall n, InP Node E n -> In Node N n)
+         /\ (forall n m, In Edge E (n,m) -> (P n m)) ->
+         ValidEdges P N E.
 
-(* Definition bundle : *)
+(* transitive closure of edges. *)
+Inductive EdgePath : Node -> Node -> Prop :=
+| path_comm : forall x y, CommEdge x y -> EdgePath x y
+| path_pred : forall x y, PredPath x y -> EdgePath x y
+| path_comm_path : forall x y z, CommEdge x y -> 
+                                 EdgePath y z -> 
+                                 EdgePath x z
+| path_pred_path : forall x y z, PredPath x y -> 
+                                 EdgePath y z -> 
+                                 EdgePath x z.
+Hint Constructors EdgePath.
 
-Definition EdgesWitness N2 P :=
- (forall n m, In (Node*Node) N2 (n,m) -> P n m).
+(* transitive reflexiv closure of edges. *)
+Inductive EdgePathEq : Node -> Node -> Prop :=
+| path_eq_refl : forall x, EdgePathEq x x
+| path_eq_path : forall x y, EdgePath x y -> EdgePathEq x y.
+Hint Constructors EdgePathEq.
 
-(* *)
+
+Inductive Acyclic_Node : Node -> Prop :=
+| acyc_node : forall n,  ~(EdgePath n n) -> Acyclic_Node n.
+
+
+Inductive Acyclic_Nodes : Nodes -> Prop :=
+| acyclic_nodes : forall N,  
+                    (forall n, In Node N n -> 
+                                Acyclic_Node n) -> Acyclic_Nodes N.
+
+
+Lemma acyc_imp_p_path_nrefl : forall n m,
+(forall x, Acyclic_Node x) ->
+PredPath n m -> n <> m.
+Proof.
+  intros n m Hacyc Hpath.
+  induction Hpath.
+  
+  apply p_edge_nrefl. auto.
+
+  remember (ppath_path i j k H Hpath) as Hik.
+  intros contra.
+  destruct (Hacyc k) as [n Hnp].
+  apply Hnp. remember (path_pred i n Hik) as Hin.
+  assert (EdgePath n n = EdgePath i n). subst. reflexivity.
+  rewrite H0. auto. 
+Qed.
+Hint Resolve acyc_imp_p_path_nrefl.
+
+
+(* An rx implies the existance of a tx *)
+Definition TxExists (N:Nodes) (CE:Edges) :=
+  (forall y m, Node_smsg y = rx m -> 
+               In Node N y ->  
+               (exists x, (In Node N x
+                           /\ Node_smsg x = tx m
+                           /\ CommEdge x y
+                           /\ In Edge CE (x,y)))).
+
+(* A node can rx a msg from only one tx at a time *)
+Definition UniqTx (N:Nodes) :=
+  (forall x y z, In Node N x ->
+                 In Node N y ->
+                 In Node N z ->
+                 CommEdge x z ->
+                 CommEdge y z ->
+                 x = y).
+
+Definition ExistsUniqTx (N:Nodes)  (CE:Edges) :=
+  TxExists N CE /\ UniqTx N.
+
+(* predecessor nodes on the same strand are also
+    in the node set *)
+Definition PredIsMember (N:Nodes) :=
+  (forall x y, In Node N y ->
+               PredEdge x y ->
+               In Node N x).
+Hint Unfold PredIsMember.
+
+(* budle definition *)
 Inductive Bundle : Nodes -> Edges -> Edges -> Prop :=
-| ssbundle : forall (N:Nodes) (CE PE:Edges),
-               Finite Node N ->
-               ValidEdges N CE ->
-               ValidEdges N PE ->
-               EdgesWitness CE CommEdge ->
-               EdgesWitness PE PredEdge ->
-               Acyclic N -> 
+| bundle : forall (N:Nodes) (CE PE:Edges),
+               ValidNodes N (Union Edge CE PE) ->
+               ValidEdges CommEdge N CE ->
+               ValidEdges PredEdge N PE ->
+               ExistsUniqTx N CE ->
+               PredIsMember N ->
+               Acyclic_Nodes N -> 
                Bundle N CE PE.
 (* [REF 1] Definition 2.4 pg 7 
     1) The subgraph is finite
@@ -315,14 +435,50 @@ Inductive Bundle : Nodes -> Edges -> Edges -> Prop :=
        to n2
     3) If n2 is in Nodes and PredEdge n1 n2 then that edge
        is in PE.
-    4) The subgraph is acyclic 
-    
-    -Additionally
-     - A strand may send or tx or rx a msg, but not 
-       both simultaneously (given by def of SMsg)
-     - When a strand rx a msg, there is a unique node 
-       tx that msg
-     - when a strand transmits a msg, many strands may
-       immediately rx that msg
+    4) The subgraph is acyclic *)
+Hint Constructors Bundle Acyclic_Node Acyclic_Nodes EdgePathEq EdgePath ValidEdges ValidNodes InP PredPath
+  PredEdge CommEdge UniqOrigin OriginateAt EntryPoint OccursIn.
+Hint Unfold PredIsMember ExistsUniqTx UniqTx TxExists Node reflexive antisymmetric transitive.
+
+Inductive NodeInBundle : Node -> Nodes -> Edges -> Edges -> Prop :=
+| node_in_bundle : forall n N CE PE,
+Bundle N CE PE -> In Node N n -> NodeInBundle n N CE PE.
+
+Lemma bundle_edge_refl : forall N CE PE,
+Bundle N CE PE -> reflexive Node EdgePathEq.
+Proof.
+  auto.
+Qed.
+
+Lemma bundle_edge_asymm : forall N CE PE,
+Bundle N CE PE -> antisymmetric Node EdgePathEq.
+  intros.
+  unfold antisymmetric.
+  intros x y Hxy Hyx.
+  destruct Hxy. auto.
+  destruct Hyx. auto.
+  Admitted. (* TODO *)
+
+  
+Lemma bundle_edge_trans : forall N CE PE,
+Bundle N CE PE -> transitive Node EdgePathEq.
+Proof.
+  Admitted. (* TODO *)
+
+(* Partial ordering of Bundle via edges *)
+Theorem BundleEdgePoset : forall N CE PE,
+Bundle N CE PE -> 
+reflexive Node EdgePathEq 
+/\ antisymmetric Node EdgePathEq
+/\ transitive Node EdgePathEq.
+Proof.
+  Admitted.
+  (* TODO *)
+  
+
+
+(* Lemma 2.7 Suppose C is a bundle. 
+   Then EdgePathEq for Nodes in C is a partial order, i.e. a reflexive, antisymmetric, 
+   transitive relation. Every non-empty subset of the nodes in 
+   C has C -minimal members.
 *)
- 
